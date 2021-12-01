f675a9eaed489f4d24c0e6e0dcef4cc73e6351fc
Varun <nurav2037@gmial.com>
2021-12-01 11:31:15 +0530

    ui

--- src/homeComponents/navbar/Navbar.js
+++ src/homeComponents/navbar/Navbar.js
@@ -1,28 +1,11 @@
 import React from 'react';
 import './Navbar.css'
-import {Navbar,Container} from 'react-bootstrap'
-import { Link } from 'react-router-dom';
 
 function Navbar1() {
     return(
-      
-        <Navbar bg="dark" variant="dark" fixed="top">
-        <Container>
-          <Navbar.Brand href="#home">
-            {/* <img
-              alt=""
-              src="/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{' '} */}
-          Thambi Services (billing)
-          </Navbar.Brand>
-          <Link to="/">
-              Home
-          </Link>
-        </Container>
-      </Navbar>
+      <div className="main--nav">
+        <h2>Thambi Services(Billing)</h2>
+      </div>
     )
 }
 
